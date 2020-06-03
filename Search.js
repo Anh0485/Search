9d0096cefcb1a2bfb975407add2b0dc92aa95bdc
Anh  Pham <contact.vananh0505@gmail.com>
2020-06-03 15:01:45 +0700

    first commit

---
+++ Search.js
@@ -0,0 +1,232 @@
+import React from 'react'
+import '../../sass/Layout/_Search.scss'
+export default function Search() {
+    return (
+
+        <div class="main-container col1-layout">
+            <div class="breadcrumbs" itemprop="breadcrumb">
+                <ul>
+                    <li class="home">
+                        <a href="https://www.cgv.vn/default/" title="Trở về trang chủ">Trang chủ</a>
+                        <span>/ </span>
+                    </li>
+                    <li class="search">
+                        <strong>Kết quả tìm theo: 'ma'</strong>
+                    </li>
+                </ul>
+            </div>
+            <div class="main">
+                <div class="col-main">
+                    <div class="page-title">
+                        <h1>Kết quả tìm theo 'ma'</h1>
+                    </div>
+                    <div class="category-products cgv-movies  ">
+                        <ul class="products-grid row">
+                            <li class=" film-lists item last col-4">
+                                <span class="nmovie-rating nmovie-rating-no">NO</span>
+                                <div class="ribon position-1"></div>
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/sky-tour-mtp.html" title="SKY TOUR" class="product-image">
+                                        <img id="product-collection-image-2748" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/a/r/artboard_1_1.jpg" alt="SKY TOUR" />
+                                    </a>
+                                    <div class="movie-technology"></div>
+                                </div>
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/sky-tour-mtp.html" title="SKY TOUR">SKY TOUR</a></h2>
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Hòa nhạc</span>
+                                    </div>
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal"></span>
+                                    </div>
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">12-06-2020</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" col-4 film-lists item last">
+                                <span class="nmovie-rating nmovie-rating-c13">C13</span>
+
+                                <div class="ribon position-2"></div>
+
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/fukushima.html" title="FUKUSHIMA 50: THẢM HỌA KÉP" class="product-image">
+                                        <img id="product-collection-image-2740" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/f/u/fukushima_50_-_payoff_poster_12062020_-_fb_1.jpg" alt="FUKUSHIMA 50: THẢM HỌA KÉP" />
+                                    </a>
+
+                                    <div class="movie-technology">
+                                    </div>
+                                </div>
+
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/fukushima.html" title="FUKUSHIMA 50: THẢM HỌA KÉP">FUKUSHIMA 50: THẢM HỌA KÉP</a></h2>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Tâm Lý</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal">121 phút</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">12-06-2020					</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" col-4 film-lists item last">
+                                <span class="nmovie-rating nmovie-rating-no">NO</span>
+
+                                <div class="ribon position-3"></div>
+
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/wishmas-tree.html" title="GIẢI CỨU CÂY ƯỚC NGUYỆN " class="product-image">
+                                        <img id="product-collection-image-2736" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/d/_/d_main-poster_wt_1__1.jpg" alt="GIẢI CỨU CÂY ƯỚC NGUYỆN " />
+                                    </a>
+
+                                    <div class="movie-technology">
+                                    </div>
+                                </div>
+
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/wishmas-tree.html" title="GIẢI CỨU CÂY ƯỚC NGUYỆN ">GIẢI CỨU CÂY ƯỚC NGUYỆN </a></h2>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Hoạt Hình</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal"></span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">12-06-2020					</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" col-4 film-lists item last">
+                                <span class="nmovie-rating nmovie-rating-c18">C18</span>
+
+                                <div class="ribon position-4"></div>
+
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/can-phong-dam-mau.html" title="CĂN PHÒNG ĐẪM MÁU" class="product-image">
+                                        <img id="product-collection-image-2732" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/m/a/main-poster_1__1.jpg" alt="CĂN PHÒNG ĐẪM MÁU" />
+                                    </a>
+
+                                    <div class="movie-technology">
+                                    </div>
+                                </div>
+
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/can-phong-dam-mau.html" title="CĂN PHÒNG ĐẪM MÁU">CĂN PHÒNG ĐẪM MÁU</a></h2>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Kinh Dị</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal">100 phút</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">05-06-2020					</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" col-4 film-lists item last">
+                                <span class="nmovie-rating nmovie-rating-no">NO</span>
+
+                                <div class="ribon position-5"></div>
+
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/peninsula.html" title="BÁN ĐẢO PENINSULA" class="product-image">
+                                        <img id="product-collection-image-2728" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/p/e/peninsula_teaser_poster_1_1__1.jpg" alt="BÁN ĐẢO PENINSULA" />
+                                    </a>
+
+                                    <div class="movie-technology">
+                                    </div>
+                                </div>
+
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/peninsula.html" title="BÁN ĐẢO PENINSULA">BÁN ĐẢO PENINSULA</a></h2>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Hành Động, Kinh Dị, Phiêu Lưu</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal"></span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">07-08-2020					</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" col-4 film-lists item last">
+                                <span class="nmovie-rating nmovie-rating-no">NO</span>
+
+                                <div class="ribon position-6"></div>
+
+                                <div class="product-images">
+                                    <a href="https://www.cgv.vn/default/tiec-trang-mau.html" title="TIỆC TRĂNG MÁU" class="product-image">
+                                        <img id="product-collection-image-2724" src="https://www.cgv.vn/media/catalog/product/cache/1/small_image/190x260/052b7e4a4f6d2886829431e534ef7a43/r/s/rsz_ttm_teaser_poster_1.jpg" alt="TIỆC TRĂNG MÁU" />
+                                    </a>
+
+                                    <div class="movie-technology">
+                                    </div>
+                                </div>
+
+                                <div class="product-info" >
+                                    <h2 class="product-name"><a href="https://www.cgv.vn/default/tiec-trang-mau.html" title="TIỆC TRĂNG MÁU">TIỆC TRĂNG MÁU</a></h2>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thể loại: </span>
+                                        <span class="cgv-info-normal">Hài, Tâm Lý</span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Thời lượng: </span>
+                                        <span class="cgv-info-normal"></span>
+                                    </div>
+
+                                    <div class="cgv-movie-info">
+                                        <span class="cgv-info-bold">Khởi chiếu: </span>
+                                        <span class="cgv-info-normal">28-08-2020					</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
